8da8520cfd1673e20addf04a2572fe6acb1433ea
Tatiana3490 <tatiana.alcubilla.m@gmail.com>
2025-01-31 12:34:17 +0100

    añado codigo del Author Exception

--- src/main/java/com/svalero/BookHub/exception/AuthorNotFoundException.java
+++ src/main/java/com/svalero/BookHub/exception/AuthorNotFoundException.java
@@ -1,4 +1,7 @@
 package com.svalero.BookHub.exception;
 
-public class AuthorNotFoundException {
+public class AuthorNotFoundException extends Exception {
+    public AuthorNotFoundException() { super("The Author does not exist"); }
+
+    public AuthorNotFoundException(String message) { super(message); }
 }
--- src/main/java/com/svalero/BookHub/repository/BookRepository.java
+++ src/main/java/com/svalero/BookHub/repository/BookRepository.java
@@ -10,9 +10,7 @@ import java.util.List;
 @Repository
 public interface BookRepository extends CrudRepository<Book, Integer> {
 
-    List<Book> findByAuthor(String author);
-    List<Book> findByTitle(String title);
-    List<Book> findByTitleAndAuthor(String title, String author);
-    List<Book> findByTitleOrAuthor(String title, String author);
+   List<Book> findAll();
+
 
 }
