80491b27466beb0bf08fa2ac366d4095fdbfd482
Tatiana3490 <tatiana.alcubilla.m@gmail.com>
2025-01-31 11:51:27 +0100

    modifico la clase Book que tenía mal los atributos

--- src/main/java/com/svalero/BookHub/domain/Author.java
+++ src/main/java/com/svalero/BookHub/domain/Author.java
@@ -1,11 +1,12 @@
 package com.svalero.BookHub.domain;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
@@ -13,5 +14,16 @@ import lombok.NoArgsConstructor;
 @Entity(name = "Author")
 @Table(name = "authors")
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column
+    private String name;
+    @Column
+    private String surname;
+    @Column
+    private LocalDate birthdate;
+    @Column
+    private boolean active;
 
 }
