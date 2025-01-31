49063f7ed107c42afb294f4f8f44b7562ee8bb31
Laura India <a27849@svalero.com>
2025-01-31 12:36:12 +0100

    author

--- src/main/java/com/svalero/BookHub/controller/AuthorController.java
+++ src/main/java/com/svalero/BookHub/controller/AuthorController.java
@@ -1,4 +1,37 @@
 package com.svalero.BookHub.controller;
 
+import com.svalero.BookHub.domain.Author;
+import com.svalero.BookHub.service.AuthorService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/authors")
 public class AuthorController {
+    private final Logger logger = LoggerFactory.getLogger(AuthorController.class);
+    private final AuthorService authorService;
+
+    @Autowired
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    // Obtener todos los autores
+    @GetMapping
+    public ResponseEntity<List<Author>> getAllAuthors() {
+        logger.info("BEGIN getAllAuthors");
+        List<Author> authors = authorService.getAllAuthors();
+        logger.info("END getAllAuthors - Total authors fetched: {}", authors.size());
+        return new ResponseEntity<>(authors, HttpStatus.OK);
+    }
+
+
 }
--- src/main/java/com/svalero/BookHub/service/AuthorService.java
+++ src/main/java/com/svalero/BookHub/service/AuthorService.java
@@ -1,11 +1,14 @@
 package com.svalero.BookHub.service;
 
 
+import com.svalero.BookHub.domain.Author;
 import com.svalero.BookHub.repository.AuthorRepository;
 import com.svalero.BookHub.repository.BookRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AuthorService {
@@ -13,4 +16,14 @@ public class AuthorService {
     private AuthorRepository authorRepository;
     @Autowired
     private ModelMapper modelMapper;
+
+    @Autowired
+    public AuthorService(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    // Obtener todos los autores
+    public List<Author> getAllAuthors() {
+        return authorRepository.findAll();
+    }
 }
